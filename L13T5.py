ee57614d8be99f8b8146b2a2855b2f50477a6987
0skr0 <97092074+0skr0@users.noreply.github.com>
2022-12-10 06:39:34 +0700

    Add files via upload

--- L13T5.py
+++ L13T5.py
@@ -8,7 +8,8 @@ while True:
     for i in range(len(options)):
         print(str(i)+')', options[i])
     selection = int(input('Enter your selection: '))
-    tag = ['id', 'name', 'type', 'species', 'origin', 'status']
+    print('\n', end='')
+    tag = ['ID', 'name', 'type', 'species', 'origin', 'status']
     if selection == 1:
         url = 'https://rickandmortyapi.com/api/character/?status=dead'
         body = json.load(urlopen(url))
@@ -16,6 +17,7 @@ while True:
         total = 0
         results = []
         limit_page = int(input('Enter limit of pages: '))
+        print('\n', end='')
         if limit_page > max_pages:
             limit_page = max_pages
         for i in range(limit_page):
@@ -23,53 +25,54 @@ while True:
             cnt = len(body['results'])
             print('Getting page', i + 1, 'url:', url)
             for j in range(cnt):
-                text = ''
-                for k in range(len(tag)):
-                    if k < len(tag) - 1:
-                        if tag[k].__eq__('origin'):
-                            text += (tag[k] + ':' + str(body['results'][j][tag[k]]['name']) + ' - ')
-                        else:
-                            text += (tag[k] + ':' + str(body['results'][j][tag[k]]) + ' - ')
-                    else:
-                        text += (tag[k] + ':' + str(body['results'][j][tag[k]]))
-                results.append(text)
+                results.append(body['results'][j])
             nextpage = body['info']['next']
             url = nextpage
             total += cnt
-        for i in results:
-            print(i)
+        for i in range(total):
+            info = {'ID': str(results[i]['id']) + ' - ',
+                    'name': str(results[i]['name']) + ' - ',
+                    'type': str(results[i]['type']) + ' ',
+                    'species': str(results[i]['species']) + ' - ',
+                    'origin': str(results[i]['origin']['name']) + ' - ',
+                    'status': str(results[i]['status']) + '\n'}
+            for j in range(len(tag)):
+                print(str(tag[j]) + ':' + info[tag[j]], end='')
         print('Total number of', total, 'dead characters')
     elif selection == 2:
         name = str(input('Enter character to search for: '))
+        print('\n', end='')
         status = str(input('Enter living status (alive, dead, unknown): '))
+        print('\n', end='')
         url = f'https://rickandmortyapi.com/api/character/?name={name}&status={status}'
         body = json.load(urlopen(url))
         max_pages = body['info']['pages']
         limit_page = int(input('Enter limit of pages: '))
+        print('\n', end='')
         if limit_page > max_pages:
             limit_page = max_pages
         total = 0
         results = []
+        if limit_page > max_pages:
+            limit_page = max_pages
         for i in range(limit_page):
             body = json.load(urlopen(url))
             cnt = len(body['results'])
             print('Getting page', i + 1, 'url:', url)
             for j in range(cnt):
-                text = ''
-                for k in range(len(tag)):
-                    if k < len(tag) - 1:
-                        if tag[k].__eq__('origin'):
-                            text += (tag[k] + ':' + str(body['results'][j][tag[k]]['name']) + ' - ')
-                        else:
-                            text += (tag[k] + ':' + str(body['results'][j][tag[k]]) + ' - ')
-                    else:
-                        text += (tag[k] + ':' + str(body['results'][j][tag[k]]))
-                results.append(text)
+                results.append(body['results'][j])
             nextpage = body['info']['next']
             url = nextpage
             total += cnt
-        for i in results:
-            print(i)
+        for i in range(total):
+            info = {'ID': str(results[i]['id']) + ' - ',
+                    'name': str(results[i]['name']) + ' - ',
+                    'type': str(results[i]['type']) + ' ',
+                    'species': str(results[i]['species']) + ' - ',
+                    'origin': str(results[i]['origin']['name']) + ' - ',
+                    'status': str(results[i]['status']) + '\n'}
+            for j in range(len(tag)):
+                print(str(tag[j]) + ':' + info[tag[j]], end='')
         print('Total number of', total, status, name+'\'s', 'found')
     elif selection == 0:
         break
--- L13T6.py
+++ L13T6.py
@@ -15,9 +15,11 @@ search = 0
 while True:
     try:
         search = int(input('Please, give the year to search for: '))
+        print('\n', end='')
         break
     except:
         print('Faulty input, please try again.')
+
 while True:
     print('Launch information descriptor')
     launches_search = []
@@ -25,7 +27,7 @@ while True:
         years = str(launches_success[i]['date_utc']).split('-')
         if int(years[0]) == search:
             launches_search.append(launches_success[i])
-    print('We have', len(launches_search), 'to choose from.')
+    print('We have', len(launches_search), 'launch to choose from.')
     for i in range(len(launches_search) + 1):
         if i < 1:
             print(str(i - 1) + ')', 'Exit')
@@ -33,13 +35,17 @@ while True:
             print(str(i - 1) + ')', launches_search[i - 1]['date_utc'], '-', launches_search[i - 1]['name'])
     selection = int(input('Select launch to display more information: '))
     tag = ['Name', 'Flight Number', 'Date', 'Mission', 'Mission status', 'Launch Wikipedia article']
+    print('\n', end='')
     if selection != -1:
         print('------------------')
         print('')
+        detail = launches_search[selection]['details']
+        if detail is None:
+            detail = 'Missing'
         info = {'Name': launches_search[selection]['name'],
                 'Flight Number': launches_search[selection]['flight_number'],
                 'Date': launches_search[selection]['date_utc'],
-                'Mission': 'Missing',
+                'Mission': detail,
                 'Mission status': 'Successful',
                 'Launch Wikipedia article': launches_search[selection]['links']['wikipedia']}
         for i in range(len(tag)):
@@ -51,7 +57,10 @@ while True:
         for i in range(len(crews)):
             url_crews = f'https://api.spacexdata.com/v4/crew/{crews[i]["crew"]}'
             body = json.load(urlopen(url_crews))
-            print('\t Crew member', i+1, '\n\t', crews[i]['role'], body['name'], '\n\t',
-                  'Agency:', body['agency'], '\n\t', 'Wikipedia page:', body['wikipedia'], '\n')
+            print('  Crew member', i+1)
+            print(' ', crews[i]['role'], body['name'])
+            print('  Agency:'+body['agency'])
+            print('  Wikipedia page:', body['wikipedia'])
+            print('')
     else:
         break
